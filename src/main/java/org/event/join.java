67945795ad74326890ba66b166ff664756f5de65
noahggarrett <noahggarrett@protonmail.com>
2021-02-11 17:39:15 -0700

    make it so the there are fireworks when a player joins for the first time along with give them a book of the rules

--- src/main/java/org/event/join.java
+++ src/main/java/org/event/join.java
@@ -6,12 +6,16 @@ package org.event;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Particle;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.items.about_book;
 import org.items.rules_book;
+import org.main.evanserver;
 import org.mineacademy.fo.Common;
 
 public class join implements Listener {
@@ -20,12 +24,37 @@ public class join implements Listener {
 		Player player = event.getPlayer();
 		event.setJoinMessage("");
 		Common.tell(player, ChatColor.BLUE + "Welcome back " + player.getName());
+
+
+		player.getWorld().spawn(player.getLocation(), Firework.class);
 		if (!player.hasPlayedBefore()){
 			Bukkit.broadcastMessage(ChatColor.DARK_AQUA + "Everyone please welcome " + player.getName() + " to the server for the very first time");
 			about_book books = new about_book();
 			rules_book rulesBook = new rules_book();
 			rulesBook.get_rules_book(player);
+			player.sendMessage(ChatColor.RED +"Please read the rules.");
+			player.sendMessage(ChatColor.RED +"Staff members get the final say on all punishments.");
 			books.givebook(player);
+			new BukkitRunnable() {
+				/**
+				 * When an object implementing interface <code>Runnable</code> is used
+				 * to create a thread, starting the thread causes the object's
+				 * <code>run</code> method to be called in that separately executing
+				 * thread.
+				 * <p>
+				 * The general contract of the method <code>run</code> is that it may
+				 * take any action whatsoever.
+				 *
+				 * @see Thread#run()
+				 */
+				@Override
+				public void run() {
+					for(int i = 0; i<30; i++){
+						player.getWorld().spawn(player.getLocation(),Firework.class);
+
+					}
+				}
+			}.runTaskLater(evanserver.getInstance(),20*5);
 		}
 
 	}
